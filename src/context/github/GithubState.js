c290af9799f70b0310d7c66ffee124ef78e8ae02
Maxim <maxidebox@list.ru>
2022-03-12 19:42:26 +0400

    128. Настройка запросов

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -7,6 +7,10 @@ import axios from "axios";
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
+const withCreds = (url) => {
+  return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+}
+
 export const GithubState = ({children}) => {
   const initialState = {
     user: {},
@@ -20,9 +24,7 @@ export const GithubState = ({children}) => {
     setLoading();
 
     // запрос на сервер
-    const response = await axios.get(
-      `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
-    )
+    const response = await axios.get(withCreds(`https://api.github.com/search/users?q=${value}&`));
 
     dispatch({
       type: SEARCH_USERS,
@@ -32,21 +34,25 @@ export const GithubState = ({children}) => {
 
   const getUser = async (name) => {
     setLoading();
+
     // запрос на сервер
+    const response = await axios.get(withCreds(`https://api.github.com/users/${name}?`));
 
     dispatch({
       type: GET_USER,
-      payload: {}
+      payload: response.data
     });
   }
 
   const getRepos = async (name) => {
     setLoading();
+
     // запрос на сервер
+    const response = await axios.get(withCreds(`https://api.github.com/users/${name}/repos?per_page=10&`));
 
     dispatch({
       type: GET_REPOS,
-      payload: []
+      payload: response.data
     });
   }
 
