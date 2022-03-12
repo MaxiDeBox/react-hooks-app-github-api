06ffd5222547d29b8358686b8b495b42bacd7f07
Maxim <maxidebox@list.ru>
2022-03-12 15:14:32 +0400

    120. Добавление роутинга

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import {Navbar} from "./components/Navbar";
 import {Home} from "./pages/Home";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import {About} from "./pages/About";
+import {Profile} from "./pages/Profile";
 
 function App() {
   return (
-    <div className="container-fluid">
-      <Navbar />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="container-fluid">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile/:name" element={<Profile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
