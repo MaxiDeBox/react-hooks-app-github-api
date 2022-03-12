1b8606e7ab722fed9553bc8f136d60451c2d61f3
Maxim <maxidebox@list.ru>
2022-03-12 16:20:00 +0400

    124. Компонент поиска

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <div className="container">
           <Alert alert={{text: 'Test alert'}} />
           <Routes>
-            <Route path="/" exact element={<Home />} />
+            <Route path="/" exact="true" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/profile/:name" element={<Profile />} />
           </Routes>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,19 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {AlertContext} from "../context/alert/alertContext";
 
 export const Search = () => {
+  const [value, setValue] = useState('');
   const {show} = useContext(AlertContext);
+
   const onSubmit = (event) => {
-    if (event.key === 'Enter') {
-      show('This is Alert');
+    if (event.key !== 'Enter') {
+      return;
+    }
+
+    if(value.trim()) {
+      // делаем запрос
+    } else {
+      show('Введите данные пользователя!');
     }
   }
 
@@ -16,6 +24,8 @@ export const Search = () => {
          type="text"
          className="form-control"
          placeholder="Введите ник пользователя ..."
+         value={value}
+         onChange={(event) => setValue(event.target.value)}
          onKeyPress={onSubmit}
        />
      </div>
